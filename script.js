74da0d3c039aeb18899e85751e26ce162991a5c8
vivek kumar <57774129+Vivek-jpg@users.noreply.github.com>
2021-11-16 10:17:06 +0000

    fed

---
+++ script.js
@@ -0,0 +1,11 @@
+const Btn = document.querySelector('.btn1');
+const Output = document.querySelector('.output');
+
+function likeCounter(){
+  console.log("click");
+}
+
+
+
+
+Btn.addEventListener("click", likeCounter);
